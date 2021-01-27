ac4dedceb150133c1b28b0d62f5e4bfbe5d9c992
tahaga61 <tahaga61@gmail.com>
2021-01-28 02:05:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string>();
+             
+        }
+    }
+}
